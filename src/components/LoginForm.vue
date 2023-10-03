ce95e20dca2e82015559eeb8797f10c7936bf9c7
ouhoy <kimkinon@gmail.com>
2023-10-03 12:54:32 +0100

    Add signup and login form comps

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+
+import {ref} from "vue";
+
+const email = ref("")
+const password = ref("")
+
+function handleSubmit() {
+
+}
+
+
+</script>
+
+<template>
+
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email">
+    <input type="password" required placeholder="password" v-model="password">
+    <button>Sign up</button>
+  </form>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+
+import {ref} from "vue";
+
+const displayName = ref("");
+const email = ref("")
+const password = ref("")
+
+function handleSubmit() {
+
+}
+
+
+</script>
+
+<template>
+
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="display name" v-model="displayName">
+    <input type="email" required placeholder="email" v-model="email">
+    <input type="password" required placeholder="password" v-model="password">
+    <button>Sign up</button>
+  </form>
+
+</template>
+
+<style scoped>
+
+</style>
